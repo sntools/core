014be2bcefa38663d3d784e44ae6ad6f4c52eb63
Samy NAAMANI <samy@namani.net>
2015-06-26 16:36:04 +0200

    Added Superglobals : Server, Session Updated documentation

--- docs/elementlist.js
+++ docs/elementlist.js
@@ -1,3 +1,3 @@
 
 var ApiGen = ApiGen || {};
-ApiGen.elements = [["c","SNTools\\NotImplementedException"],["c","SNTools\\Object"],["c","SNTools\\PropertyException"]];
+ApiGen.elements = [["c","SNTools\\NotImplementedException"],["c","SNTools\\Object"],["c","SNTools\\PropertyException"],["c","SNTools\\Server"],["c","SNTools\\Session"],["c","SNTools\\Superglobal"]];
